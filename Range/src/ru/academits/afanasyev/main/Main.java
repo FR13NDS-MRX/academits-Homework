24b997b08affbfd2ce9ab8987c940ec10b75bff0
Ketobemidine <kirill_afan@mail.ru>
2019-07-08 22:49:47 +0600

    Добавил в Range метод для вычисления интервала-пересечения

--- Range/src/ru/academits/afanasyev/main/Main.java
+++ Range/src/ru/academits/afanasyev/main/Main.java
@@ -5,7 +5,16 @@ import ru.academits.afanasyev.range.Range;
 public class Main {
     public static void main(String[] args) {
         Range range = new Range(0, 100);
+        Range range1 = new Range(54, 187);
+        Range range2 = new Range(188, 243);
+        Range range3 = range1.getCrossingInterval(range2);
 
+        if (range3 == null) {
+            System.out.println("null");
+        } else {
+            System.out.printf("Новый интервал пересечение - от %f до %f%n", range3.getFrom(), range3.getTo());
+        }
+        
         range.setFrom(10);
         range.setTo(110);
 
--- Range/src/ru/academits/afanasyev/range/Range.java
+++ Range/src/ru/academits/afanasyev/range/Range.java
@@ -32,4 +32,32 @@ public class Range {
     public boolean isInside(double number) {
         return (number >= from) && (number <= to);
     }
+
+    public Range getCrossingInterval(Range range) {
+        double range1From = this.getFrom();
+        double range1To = this.getTo();
+        double range2From = range.getFrom();
+        double range2To = range.getTo();
+        double range3From = 0;
+        double range3To = 0;
+
+        if (range1From > range2From) {
+            range3From = range1From;
+        }
+        if (range1From < range2From) {
+            range3From = range2From;
+        }
+        if (range1To > range2To) {
+            range3To = range2To;
+        }
+        if (range1To < range2To) {
+            range3To = range1To;
+        }
+
+        if ((range3From == 0) && (range3To == 0) || (range3From > range3To)) {
+            return null;
+        } else {
+            return new Range(range3From, range3To);
+        }
+    }
 }
