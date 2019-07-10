e37517dae8d53e0a575ee6b89349b8b0565188ba
Ketobemidine <kirill_afan@mail.ru>
2019-07-10 12:11:39 +0600

    Сделал Range*

--- Range/src/ru/academits/afanasyev/main/Main.java
+++ Range/src/ru/academits/afanasyev/main/Main.java
@@ -6,15 +6,30 @@ public class Main {
     public static void main(String[] args) {
         Range range = new Range(0, 100);
         Range range1 = new Range(54, 187);
-        Range range2 = new Range(188, 243);
+        Range range2 = new Range(150, 243);
         Range range3 = range1.getCrossingInterval(range2);
+        Range[] range4 = range1.addIntervals(range2);
+        Range[] range5 = range1.subtractIntervals(range2);
 
         if (range3 == null) {
             System.out.println("null");
         } else {
-            System.out.printf("Новый интервал пересечение - от %f до %f%n", range3.getFrom(), range3.getTo());
+            System.out.printf("Новый интервал-пересечение - (%.2f; %.2f)%n", range3.getFrom(), range3.getTo());
         }
-        
+
+        if (range4[1] == null) {
+            System.out.printf("Новый интервал-объединение - (%.2f; %.2f)%n", range4[0].getFrom(), range4[0].getTo());
+        } else {
+            System.out.printf("Новый интервал-объединение - (%.2f; %.2f) и (%.2f; %.2f)%n", range4[0].getFrom(), range4[0].getTo(), range4[1].getFrom(), range4[1].getTo());
+        }
+
+        if (range5[1] == null) {
+            System.out.printf("Новый интервал-разность - (%.2f; %.2f)%n", range5[0].getFrom(), range5[0].getTo());
+        } else {
+            System.out.printf("Новый интервал-разность - (%.2f; %.2f) и (%.2f; %.2f)%n", range5[0].getFrom(), range5[0].getTo(), range5[1].getFrom(), range5[1].getTo());
+        }
+
+
         range.setFrom(10);
         range.setTo(110);
 
@@ -22,9 +37,9 @@ public class Main {
         double number = 50;
 
         if (range.isInside(number)) {
-            System.out.printf("Чилсло %f входит в диапазон от %f до %f", number, range.getFrom(), range.getTo());
+            System.out.printf("Чилсло %.2f входит в диапазон (%.2f; %.2f)", number, range.getFrom(), range.getTo());
         } else {
-            System.out.printf("Число %f не входит в диапазон от %f до %f", number, range.getFrom(), range.getTo());
+            System.out.printf("Число %.2f не входит в диапазон (%.2f;%.2f)", number, range.getFrom(), range.getTo());
         }
     }
 }
--- Range/src/ru/academits/afanasyev/range/Range.java
+++ Range/src/ru/academits/afanasyev/range/Range.java
@@ -60,4 +60,73 @@ public class Range {
             return new Range(range3From, range3To);
         }
     }
+
+    public Range[] addIntervals(Range range) {
+        double range1From = this.getFrom();
+        double range1To = this.getTo();
+        double range2From = range.getFrom();
+        double range2To = range.getTo();
+        double range4From;
+        double range4To;
+
+        if ((range1To > range2From) && (range1To < range2To)) {
+            range4From = range1From;
+            range4To = range2To;
+
+            Range range4 = new Range(range4From, range4To);
+            return new Range[]{range4, null};
+        }
+        if ((range1To > range2From) && (range1To > range2To)) {
+            range4From = range1From;
+            range4To = range1To;
+
+            Range range4 = new Range(range4From, range4To);
+            return new Range[]{range4, null};
+        }
+
+        Range range1 = new Range(range1From, range1To);
+        Range range2 = new Range(range2From, range2To);
+
+        return new Range[]{range1, range2};
+    }
+
+    public Range[] subtractIntervals(Range range) {
+        double range1From = this.getFrom();
+        double range1To = this.getTo();
+        double range2From = range.getFrom();
+        double range2To = range.getTo();
+        double range5From;
+        double range5To;
+        double range6From;
+        double range6To;
+
+        if ((range1To > range2From) && (range1To < range2To)) {
+            range5From = range1From;
+            range5To = range2From - 1;
+
+            Range range5 = new Range(range5From, range5To);
+            return new Range[]{range5, null};
+        }
+
+        if (range1To < range2From) {
+            range5From = range1From;
+            range5To = range1To;
+
+            Range range5 = new Range(range5From, range5To);
+            return new Range[]{range5, null};
+        }
+
+        if ((range1To > range2From) && (range1To > range2To)) {
+            range5From = range1From;
+            range5To = range2From - 1;
+            range6From = range2To + 1;
+            range6To = range1To;
+
+            Range range5 = new Range(range5From, range5To);
+            Range range6 = new Range(range6From, range6To);
+            return new Range[]{range5, range6};
+        }
+
+        return null;
+    }
 }
