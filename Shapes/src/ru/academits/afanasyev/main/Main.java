136ffbb0117c45c9dced129b9ef45a4c53d096c8
Ketobemidine <kirill_afan@mail.ru>
2019-08-04 00:00:43 +0700

    Shapes, часть 2 неоконченная

--- Shapes/src/ru/academits/afanasyev/main/Main.java
+++ Shapes/src/ru/academits/afanasyev/main/Main.java
@@ -1,7 +1,26 @@
 package ru.academits.afanasyev.main;
 
+import ru.academits.afanasyev.shapes.*;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        Square square = new Square(5);
+        Triangle triangle = new Triangle(1, 5, 3, 1, 2, 5);
+        Rectangle rectangle1 = new Rectangle(2, 4);
+        Rectangle rectangle2 = new Rectangle(1, 5);
+        Circle circle1 = new Circle(3);
+        Circle circle2 = new Circle(4);
 
+        double maxAreaShape = getMaxAreaShape(square.getArea(), triangle.getArea(), rectangle1.getArea(), rectangle2.getArea(), circle1.getArea(), circle2.getArea());
+        System.out.println("Максимальная площадь фигуры - " + maxAreaShape);
+
+
+    }
+
+    public static double getMaxAreaShape(double... shape) {
+        Arrays.sort(shape);
+        return shape[shape.length - 1];
     }
 }
--- Shapes/src/ru/academits/afanasyev/shapes/Triangle.java
+++ Shapes/src/ru/academits/afanasyev/shapes/Triangle.java
@@ -17,15 +17,15 @@ public class Triangle implements Shape {
         this.y3 = y3;
     }
 
-    public double getALength() {
+    private double getALength() {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
-    public double getBLength() {
+    private double getBLength() {
         return Math.sqrt(Math.pow(x3 - x1, 2) + Math.pow(y3 - y1, 2));
     }
 
-    public double getCLength() {
+    private double getCLength() {
         return Math.sqrt(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2));
     }
 
